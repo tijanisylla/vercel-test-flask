1f0174aa402fa54fb3c9956c6656b003e6085d5b
Tijani Sylla <tijani.sylla1@gmail.com>
2023-02-17 14:50:21 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/test', methods=['GET'])
+def test_get():
+    return jsonify({'message': 'Hello World!'})
+
+
+@app.route('/test2', methods=['GET'])
+def test_get_2():
+    return jsonify({'message': 'Hello World! 2'})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
